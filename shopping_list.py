6b6480bd10d43ee8fb87ac4a28da0a50f443d56f
Andrea Rus <andrearus27@gmail.com>
2021-07-03 20:45:26 +0100

    shopping list interface using tkinter

--- shopping_list.py
+++ shopping_list.py
@@ -1,7 +1,19 @@
-import os
-import urllib
-
-from google.appengine.api import users
-from google.appengine.ext import ndb
+import tkinter
+from tkinter import *
 
 shopping_list = []
+
+
+def createList(shopping):
+    for elem in shopping_list:
+        shopping_list.insert(END, elem[0] + "-" + str(elem[1]))
+
+
+root = Tk()
+root.title("Shopping List")
+
+
+addButton = Button(window, text="Add Item", padx=50, pady=5)
+addButton.pack()
+
+root.mainloop()

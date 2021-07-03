7b0cb69d5868c3e17508b637d378e5addd9afca7
Andrea Rus <andrearus27@gmail.com>
2021-07-03 23:10:37 +0100

    Update shopping_list.py

--- shopping_list.py
+++ shopping_list.py
@@ -1,19 +1,77 @@
 import tkinter
 from tkinter import *
 
-shopping_list = []
-
 
 def createList(shopping):
-    for elem in shopping_list:
-        shopping_list.insert(END, elem[0] + "-" + str(elem[1]))
+    for elem in shopping:
+        theList.insert(END, elem[0] + "-" + str(elem[1]))
 
 
-root = Tk()
-root.title("Shopping List")
+def listIndex(shopping, item):
+    index = -1
+    for i in range(len(shopping)):
+        if shopping[i][0] == item:
+            index = i
+    return index
 
 
-addButton = Button(window, text="Add Item", padx=50, pady=5)
-addButton.pack()
+def addList(shopping, item, index):
+    if index == -1:
+        shopping.append([item, 1])
 
-root.mainloop()
+
+def removeList(shopping, index):
+    del(shopping[index])
+
+
+# Function that adds an item to the list
+def add():
+    index = listIndex(shopping, item.get())
+    addList(shopping, item.get(), index)
+    if index >= 0:
+        theList.delete(index)
+        theList.insert(index, shopping[index][0])
+
+# Function to remove item from list
+
+
+def remove():
+    index = theList.index(ACTIVE)
+    print(index)
+    removeList(shopping, index)
+    theList.delete(index)
+
+
+# Empty Shopping List
+shopping = []
+
+
+# Creating the tkinter window
+window = Tk()
+window.title("Shopping List")
+window.geometry("600x600")
+
+
+# The layout for the list
+theList = Listbox(window, selectmode=MULTIPLE, width=50, height=30, bd=20)
+theList.grid(row=0, column=0, columnspan=2, sticky=E)
+
+# Function to createList is called
+createList(shopping)
+
+
+item = StringVar()
+
+# Input item
+Label(window, text="Item:").grid(row=1, column=0, sticky=E)
+Entry(window, textvariable=item).grid(row=1, column=1, sticky=W)
+
+
+# Buttons
+Button(window, text="Add Item", command=add, padx=30).grid(
+    row=3, column=0, columnspan=3)
+
+Button(window, text="Remove Item", command=remove,
+       padx=30).grid(row=3, column=3)
+
+window.mainloop()
